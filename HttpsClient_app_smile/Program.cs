a4aa82ee7bc5c89fb080aed558de188cb537724c
pianz <pianz@DESKTOP-J53T2I0>
2024-05-25 14:30:47 +0300

    Добавьте файлы проекта.

---
+++ HttpsClient_app_smile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Io
+
+namepace HttpsClient_spp_smile
+    class Program
+{
+    public static string fileOutput = "@D:\\Программы\\Программирование\\Visual studio projects\\HttpsClient_app_smile\\HttpsClient_app_smile\\TextFile1.txt";
+    public static  async Task <string> CallUrl (string url)
+    {
+        HttpClient httpClient = new HttpClient ();
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        ClientCertificateOption 
+    }
+}

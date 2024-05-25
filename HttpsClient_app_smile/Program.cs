51587b6bd508337aa3d63adb378c41d14d887b57
NRR <pianzovmaxsim236@gmail.com>
2024-05-25 14:40:00 +0300

    update#

--- HttpsClient_app_smile/Program.cs
+++ HttpsClient_app_smile/Program.cs
@@ -2,16 +2,32 @@
 using System.Threading.Tasks;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
-using System.Io
+using System.IO;
 
-namepace HttpsClient_spp_smile
+namespace HttpsClient_spp_smile;
     class Program
 {
     public static string fileOutput = "@D:\\Программы\\Программирование\\Visual studio projects\\HttpsClient_app_smile\\HttpsClient_app_smile\\TextFile1.txt";
+    static void main(string[] args)
+    {
+        Console.Write("Вставь ссылку!");
+        string url = Console.ReadLine();
+        var awaiter = CallUrl(url);
+        if (awaiter.Result !="") 
+        {
+            File.WriteAllText(fileOutput, awaiter.Result);
+            Console.WriteLine("tHML Response output to " + fileOutput);
+        }
+        Console.WriteLine("Нажми любую кнопку");
+    }
+        
+
     public static  async Task <string> CallUrl (string url)
     {
         HttpClient httpClient = new HttpClient ();
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-        ClientCertificateOption 
+        client.DefaultRequestHeaders.Accespt.Clear();
+        var respons = client.GetstringAsync (url).Result;
+        return  await respons;
     }
 }

74cc4aa938a8bfa60b1b9e175cf0bf8fc5805b30
NRR <pianzovmaxsim236@gmail.com>
2024-05-25 14:49:44 +0300

    fix

--- HttpsClient_app_smile/Program.cs
+++ HttpsClient_app_smile/Program.cs
@@ -8,7 +8,7 @@ namespace HttpsClient_spp_smile
  class Program
     {
     public static string fileOutput = "@D:\\Программы\\Программирование\\Visual studio projects\\HttpsClient_app_smile\\HttpsClient_app_smile\\TextFile1.txt";
-    static void main(string[] args)
+        static void Main(string[] args)
     {
         Console.Write("Вставь ссылку!");
         string url = Console.ReadLine();
